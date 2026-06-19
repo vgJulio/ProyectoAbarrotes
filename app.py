e71d93a0209daf02f640bd0b59a71d2dab08fb54
josueURP <202211306@urp.edu.pe>
2026-06-19 02:07:39 -0500

    Ahora es un panel para admin, tacho drag&drop y conexion a mongo para eliminar, editar desde la página.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, redirect, url_for
 from models.conexion import db
+from bson import ObjectId
+
 
 app = Flask(__name__)
 
@@ -38,8 +42,42 @@ def login():
 def clienteVista():
     return render_template('clienteVista.html')
 
-@app.route('/inventarioVista')
+@app.route('/inventarioVista', methods=['GET', 'POST'])
 def inventarioVista():
+    if request.method == 'POST':
+        producto_id = request.form.get('producto_id')
+        nombre = request.form.get('nombre')
+        categoria = request.form.get('categoria')
+        precio = float(request.form.get('precio'))
+        stock = int(request.form.get('stock'))
+        descripcion = request.form.get('descripcion')
+
+        imagen_archivo = request.files.get('imagen')
+        nombre_imagen = None
+        
+        if imagen_archivo and imagen_archivo.filename != '':
+            nombre_imagen = secure_filename(imagen_archivo.filename)
+            ruta_guardado = os.path.join(app.root_path, 'static', 'img', nombre_imagen)
+            imagen_archivo.save(ruta_guardado)
+
+        datos_producto = {
+            'nombre': nombre,
+            'categoria': categoria,
+            'precio': precio,
+            'stock': stock,
+            'descripcion': descripcion
+        }
+        
+        if nombre_imagen:
+            datos_producto['imagen'] = nombre_imagen
+
+        if producto_id:
+            db.productos.update_one({'_id': ObjectId(producto_id)}, {'$set': datos_producto})
+        else:
+            db.productos.insert_one(datos_producto)
+            
+        return redirect(url_for('inventarioVista'))
+
     productos = list(db.productos.find({}, {
         'nombre': 1,
         'precio': 1,
@@ -49,6 +87,14 @@ def inventarioVista():
         'imagen': 1
     }))
     return render_template('inventarioVista.html', productos=productos)
+
+@app.route('/eliminar_producto/<id>', methods=['DELETE'])
+def eliminar_producto(id):
+    try:
+        db.productos.delete_one({'_id': ObjectId(id)})
+        return 'Producto eliminado', 200
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
